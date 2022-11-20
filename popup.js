01d5544589eae5b89047437de5479e8457f88793
automaton <automaton@datascape.org>
2022-11-19 23:00:50 -0500

    Get extension basic functionality working. Search and loading of results in tabs

--- popup.js
+++ popup.js
@@ -22,28 +22,47 @@ function isMatch(searchStr, res){
 
 function getMatches(lsRes, searchStr){
 
+  var searchStr = document.getElementById("searchTerms").value.toLowerCase()
+
+  doTokenize=false
   if( document.getElementById("searchTypeAny").checked ){
-    searchStr = searchStr.replaceAll(" ", "|") //pipe treats tokens as or
+    searchStr = searchStr.replaceAll(" ", "|"); //pipe treats tokens as or
   }
   else if ( document.getElementById("searchTypeExact").checked ) {
-    searchStr = '="' + searchStr + '"'
+    searchStr = '="' + searchStr + '"';
   }
-  console.log(searchStr)
-  // Otherwise leave as is with spaces acting as "AND"
+  else{
+    doTokenize=true
+  }
 
+  // Otherwise leave as is with spaces acting as "AND"
+  console.log("Search term:" + searchStr)
+  
   const options = {
     includeScore: true,
     isCaseSensitive: false,
     minMatchCharLength: 3,
     useExtendedSearch: true,
-    threshold: 0.3,
+    ignoreLocation: true,
+    tokenize: doTokenize,
+    threshold: 0.1,
     // Search in `author` and in `tags` array
     keys: ['title', 'parents']
+  };
+
+  const fuse = new Fuse(lsRes, options);
+
+  jsonMatches = fuse.search(searchStr);
+
+  var lsMatches = []
+  for(var iMatch = 0; iMatch < jsonMatches.length; iMatch++){
+    match = jsonMatches[iMatch];
+    item = match["item"]
+    console.log(match)
+    lsMatches.push({title: item["title"], url: item["url"], tags: item["parents"], score: match["score"]});
   }
 
-  const fuse = new Fuse(lsRes, options)
-
-  return fuse.search(searchStr)
+  return lsMatches;
 }
 
 
@@ -51,40 +70,40 @@ function getMatches(lsRes, searchStr){
 function addToSearchResults(lsMatches){
   var searchResults = document.getElementById("searchResultsTable")
 
-  var searchStr = document.getElementById("searchTerms").value.toLowerCase()
-  console.log("Search term:" + searchStr)
-
   tableInner = ""
 
-  maxParents=0;
+  maxTags=0;
   for(i = 0; i<lsMatches.length; i++){
-    if(lsMatches[i].parents.length > maxParents){
-      maxParents=lsMatches[i].parents.length
+    if(lsMatches[i].tags.length > maxTags){
+      maxTags=lsMatches[i].tags.length-1
     }
   }
 
   //Add table header up to maximum depth
-  tableInner += "<tr><th>Bookmark</th>"
-  for(i = 0; i < maxParents; i++){
+  tableInner += "<tr><th>Score</th><th>Bookmark</th>"
+  for(i = 0; i < maxTags; i++){
     tableInner += "<th>" + "L" + i + "</th>"
   }
   tableInner+= "</tr>"
 
   for(i = 0; i<lsMatches.length; i++){
-    console.log(title)
-    tableInner += "<tr>"
+    tableInner += "<tr>";
 
-    tableInner += "<td>"
-    tableInner += "<a href='" + lsMatches[i].url + "'>" + lsMatches[i].title + "</a><br>"
-    tableInner += "</td>"
-
-    for(j = 0; j<lsMatches[i].parents.length; j++){
-      if(lsMatches[i].parents[j] != ""){
-        tableInner += "<td><div class='chip'>" + lsMatches[i].parents[j] +
-            "<span class='closebtn'>&times;</span></div></td>"
+    //Print score
+    var score = lsMatches[i].score;
+    tableInner += "<td>" + score.toFixed(3) + "</td>";
+    //Print link to bookmark
+    tableInner += "<td>";
+    tableInner += "<a href='" + lsMatches[i].url + "'>" + lsMatches[i].title + "</a><br>";
+    tableInner += "</td>";
+    //Print tags
+    for(j = 0; j<lsMatches[i].tags.length; j++){
+      if(lsMatches[i].tags[j] != ""){
+        tableInner += "<td><div class='chip'>" + lsMatches[i].tags[j] +
+            "<span class='closebtn'>&times;</span></div></td>";
       }
     }
-    tableInner += "</tr>"
+    tableInner += "</tr>";
   }
 
   searchResults.innerHTML = tableInner
@@ -93,33 +112,42 @@ function addToSearchResults(lsMatches){
 // Get all bookmarks and filter by search term
 document.getElementById("searchBookmarksButton").onclick = function() {
   console.log("Sending message to get bookmarks")
-  const handler = chrome.runtime.sendMessage({action: "get-bookmarks"})
-
-  handler.then(function(response){
-    console.log("Received bookmarks:" + response.bookmarks);
+  const handler = chrome.runtime.sendMessage({action: "get-bookmarks"}, response => {
 
     var searchStr = document.getElementById("searchTerms").value
     lsMatches = getMatches(response.bookmarks, searchStr)
-    console.log("Matches:" + lsMatches)
+    //console.log("Matches:" + lsMatches)
     addToSearchResults(lsMatches)
-  },
-  function(error) {
-    console.log("Error: " + error)
+
+    //Save search results
+    chrome.storage.local.set({ "searchMatches": lsMatches }, function(){
+      console.log("Saving last search" + lsMatches)
   });
+  })
 }
 
-document.querySelector('#loadSearchResults').addEventListener("onclick", async () => {
+document.getElementById("loadSearchResults").onclick = function() {
 
-  let tabsIds = [];
+  console.log("Load search results in tabs");
+  chrome.storage.local.get(["searchMatches"], function(result) {
+      console.log('Fetching last search' + result.searchMatches);
 
-  console.log("Curr: " + lsCurrRes.length)
-  for(i=0; i<lsCurrRes.length; i++){
-    const tab = chrome.tabs.create({ url: lsCurrRes[i].url, active: false, selected: false })
-    tabsIds.push(tab.id);
-  }
+      //let tabsIds = [];
 
-  const groupId = await chrome.tabs.group({tabIds: tabsIds});
-});
+      //console.log("Curr: " + lsCurrRes.length)
+      //for(i=0; i<lsCurrRes.length; i++){
+      //  const tab = chrome.tabs.create({ url: lsCurrRes[i].url, active: false, selected: false })
+      //  tabsIds.push(tab.id);
+      //}
+
+      //const groupId = await chrome.tabs.group({tabIds: tabsIds});
+      for(var i=0; i<result.searchMatches.length; i++){
+        chrome.tabs.create({
+          url: result.searchMatches[i].url
+        });
+      }
+    });
+}
 
 // Tree view control (cleanup)
 var toggler = document.getElementsByClassName("caret");
--- src/background.js
+++ src/background.js
@@ -1,22 +1,34 @@
 // List of all bookmarks
 console.log('Starting background service worker');
 
-let lsAllBookmarks = []
+let lsAllBookmarks = [];
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === "get-bookmarks") {
-        chrome.bookmarks.getTree(function(tree) {
-          console.log("Tree:" + tree)
-          genListBookmarks(tree, lsAllBookmarks, []);
-          console.log("Bookmarks:" + lsAllBookmarks)
-          sendResponse({ bookmarks: lsAllBookmarks });
-        });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "get-bookmarks") {  
+        console.log("Processing get-bookmarks");
+        fetchBookmarks(request).then(sendResponse);
+        return true;
     }
 });
 
+function fetchBookmarks(request) {
+    return new Promise(resolve => {
+        //Call Chrome API to get all books
+        chrome.bookmarks.getTree(function(tree){
+            
+            lsAllBookmarks = [];
+
+            //Flatten into a list
+            genListBookmarks(tree, lsAllBookmarks, []);
+            //Return to caller 
+            resolve({bookmarks: lsAllBookmarks});
+        });
+    })
+}
+
 // Recursively scan the tree of bookmarks and build a simple list
 function genListBookmarks(bookmarks, lsBookmarks, parents) {
-    for (let i = 0; i < bookmarks.length; i++) {
+    for (var i = 0; i < bookmarks.length; i++) {
         const bookmark = bookmarks[i];
         if (bookmark.url) {
             lsBookmarks.push({
@@ -27,13 +39,13 @@ function genListBookmarks(bookmarks, lsBookmarks, parents) {
             //console.log("bookmark: " + bookmark.title + " ~  " + bookmark.url);
         }
         if (bookmark.children) {
-            // Exclude the "Bookmark Bar" (top level) since it adds no context
-            if (bookmark.title != "Bookmarks Bar"){
-              currParent = []
-              if(parents.length > 0){
+            currParent = [];
+            if(parents.length > 0){
                 currParent = parents.map((x) => x);
-              }
-              currParent.push(bookmark.title)
+            }
+            // Exclude the "Bookmark Bar" (top level) since it adds no context
+            if (bookmark.title != "Bookmarks bar"){
+              currParent.push(bookmark.title);
             }
             genListBookmarks(bookmark.children, lsBookmarks, currParent);
         }
